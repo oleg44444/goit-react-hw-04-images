a65e42413ecd638d25cb2db8963105a88e0081cd
Oleg Gonchar <oleg.vn89@gmail.com>
2023-10-12 00:42:51 +0300

    refactoring
    transfer func load image in app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,58 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import { Container } from './App.styled';
 
 export const App = () => {
   const [text, setText] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const loadImages = () => {
+    setIsLoading(true);
+
+    axios
+      .get('https://pixabay.com/api/', {
+        params: {
+          q: text,
+          page: page,
+          per_page: 12,
+          key: '38612170-77e451be80bcbbe7a33b7fee0',
+        },
+      })
+      .then(response => {
+        setImages(prevImages => [...prevImages, ...response.data.hits]);
+        setPage(prevPage => prevPage + 1);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Помилка при отриманні даних:', error);
+        setIsLoading(false);
+      });
+  };
 
   const onSubmitForm = data => {
     setText(data);
+    setPage(1);
+    setImages([]);
+    loadImages();
   };
+
   return (
     <Container>
       <Searchbar onSubmit={onSubmitForm} />
-      <ImageGallery search={text} />
+      <ImageGallery
+        search={text}
+        images={images}
+        loadImages={loadImages}
+        page={page}
+        isLoading={isLoading}
+        selectedImage={selectedImage}
+        setSelectedImage={setSelectedImage}
+      />
     </Container>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,48 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
 import Loader from '../Loader/Loader';
 import Modal from '../Modal/Modal';
 import { List } from './ImageGallery.styled';
 
-const ImageGallery = ({ search }) => {
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  useEffect(() => {
-    if (search.length > 0) {
-      setPage(1);
-      setImages([]);
-      loadImages();
-    }
-  }, [search]);
-
-  const loadImages = () => {
-    setIsLoading(true);
-
-    axios
-      .get('https://pixabay.com/api/', {
-        params: {
-          q: search,
-          page: page,
-          per_page: 12,
-          key: '38612170-77e451be80bcbbe7a33b7fee0',
-        },
-      })
-      .then(response => {
-        setImages(prevImages => [...prevImages, ...response.data.hits]);
-        setPage(prevPage => prevPage + 1);
-        setIsLoading(false);
-      })
-      .catch(error => {
-        console.error('Помилка при отриманні даних:', error);
-        setIsLoading(false);
-      });
-  };
-
+const ImageGallery = ({
+  search,
+  images,
+  loadImages,
+  page,
+  isLoading,
+  selectedImage,
+  setSelectedImage,
+}) => {
   const handleImageClick = selectedImage => {
     setSelectedImage(selectedImage);
   };
